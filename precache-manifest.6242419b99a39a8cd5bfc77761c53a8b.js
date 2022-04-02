da9ebb399481045660b253b9e589764953c4a268
leo1p <leo1pacheco@hotmail.com>
2022-04-01 19:50:00 -0500

    Updates

---
+++ precache-manifest.6242419b99a39a8cd5bfc77761c53a8b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e49d16619c779fb51b",
+    "url": "/TWPL/static/js/0.a644bc30.chunk.js"
+  },
+  {
+    "revision": "929ec2b4913a0b761b17",
+    "url": "/TWPL/static/css/main.0ee976a0.chunk.css"
+  },
+  {
+    "revision": "929ec2b4913a0b761b17",
+    "url": "/TWPL/static/js/main.05a6d96f.chunk.js"
+  },
+  {
+    "revision": "3d1312fbf9b90ad9c6ad",
+    "url": "/TWPL/static/js/runtime~main.fddf270e.js"
+  },
+  {
+    "revision": "9d44147bd4f27f9c15c0",
+    "url": "/TWPL/static/js/3.99c1ac4a.chunk.js"
+  },
+  {
+    "revision": "2b524f2033d1a9123b70",
+    "url": "/TWPL/static/js/4.790b0e1d.chunk.js"
+  },
+  {
+    "revision": "45df3c25dc1532b98107",
+    "url": "/TWPL/static/js/5.78c0ff1e.chunk.js"
+  },
+  {
+    "revision": "7d4306860ea17e80378d",
+    "url": "/TWPL/static/css/6.59fc1f04.chunk.css"
+  },
+  {
+    "revision": "7d4306860ea17e80378d",
+    "url": "/TWPL/static/js/6.45b21bc9.chunk.js"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/TWPL/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/TWPL/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/TWPL/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/TWPL/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "84c6fe1e4353c916d8f1452e82bdef8d",
+    "url": "/TWPL/static/media/materialdesignicons-webfont.84c6fe1e.woff2"
+  },
+  {
+    "revision": "1a95531b09ffc9b76104e0003b456a0e",
+    "url": "/TWPL/static/media/materialdesignicons-webfont.1a95531b.woff"
+  },
+  {
+    "revision": "be7691c114e7c9d0543b89e824c4003d",
+    "url": "/TWPL/static/media/materialdesignicons-webfont.be7691c1.eot"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/TWPL/static/media/materialdesignicons-webfont.6acc7d6c.ttf"
+  },
+  {
+    "revision": "51b68d0604660b53c44400ccdef379c0",
+    "url": "/TWPL/static/media/materialdesignicons-webfont.51b68d06.svg"
+  },
+  {
+    "revision": "8b073cf702bddf835869b171fe03d73d",
+    "url": "/TWPL/static/media/img-1.8b073cf7.jpg"
+  },
+  {
+    "revision": "80e52295922c70fdaa79d54afe26e883",
+    "url": "/TWPL/static/media/bg-home.80e52295.jpg"
+  },
+  {
+    "revision": "d0b8f70643b398774a14ebe4e56bbdcb",
+    "url": "/TWPL/static/media/img-2.d0b8f706.jpg"
+  },
+  {
+    "revision": "07c9babb2e101fa952538a5cf85258c5",
+    "url": "/TWPL/static/media/logo.07c9babb.png"
+  },
+  {
+    "revision": "058968437a6816318c07446758d51df7",
+    "url": "/TWPL/static/media/img-1.05896843.jpg"
+  },
+  {
+    "revision": "b2926a4cba8de7a66bc6d3e07c0b4682",
+    "url": "/TWPL/static/media/img-2.b2926a4c.jpg"
+  },
+  {
+    "revision": "23d0cae639e952805c67d63803ee81c8",
+    "url": "/TWPL/static/media/img-3.23d0cae6.jpg"
+  },
+  {
+    "revision": "41b51894b16a8fa4cabb3190cd9efda0",
+    "url": "/TWPL/static/media/img-4.41b51894.jpg"
+  },
+  {
+    "revision": "82823d2d255c1aaa9df37508847fd56f",
+    "url": "/TWPL/static/media/img-5.82823d2d.jpg"
+  },
+  {
+    "revision": "4bd22c934462158e687e621989e88631",
+    "url": "/TWPL/static/media/img-6.4bd22c93.jpg"
+  },
+  {
+    "revision": "d0136eef8937b403c44d5d2b1f3431ca",
+    "url": "/TWPL/static/media/img-7.d0136eef.jpg"
+  },
+  {
+    "revision": "228be381fed4705505eba3b7870ceacc",
+    "url": "/TWPL/static/media/img-8.228be381.jpg"
+  },
+  {
+    "revision": "13ceea236921c801923f8b702653988a",
+    "url": "/TWPL/static/media/img-9.13ceea23.jpg"
+  },
+  {
+    "revision": "4e66c63b126977c1abce89e29ffd0669",
+    "url": "/TWPL/index.html"
+  }
+];
